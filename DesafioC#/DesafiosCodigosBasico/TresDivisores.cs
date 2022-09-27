8c2f7cdc033dd71580ff3309a03390b56faa7375
Felipe Menegueli <felipemenegueli08@gmail.com>
2022-09-27 13:16:33 -0300

    Novo desafio tres divisores

--- DesafioC#/DesafiosCodigosBasico/TresDivisores.cs
+++ DesafioC#/DesafiosCodigosBasico/TresDivisores.cs
@@ -13,3 +13,22 @@ Explicação: 2 possui apenas dois divisores: 1 e 2.
 
 */
 
+using System;
+ 
+class Solution {
+    public static void Main(string[] args) {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+ 
+        for (int i = 1; i <= n; i++) {
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+            if (n % i == 0) {
+                count++;
+            }
+            if (count > 3) {
+                Console.WriteLine(false);
+            }
+        }
+         Console.WriteLine(count == 3);
+    }
+}
