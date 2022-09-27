1aaa10c7b4192ea90cd3b723f637fcfccf6ae5f9
Felipe Menegueli <felipemenegueli08@gmail.com>
2022-09-27 12:51:21 -0300

    Adicionado novos desafios

---
+++ DesafioC#/DesafiosCodigosBasico/TresDivisores.cs
@@ -0,0 +1,15 @@
+/*
+Dado um inteiro n, retorne true se n tiver exatamente três divisores positivos. Caso contrário, retorne false. 
+O inteiro m é um divisor de n, se existe um inteiro k tal que n = k * m. 
+
+O arquivo de entrada consiste em um inteiro n que será validada caso esteja sob as condições do desafio.
+
+A saída consistirá em um tipo booleano: true ou false. Como nos exemplos a baixo:
+
+Exemplo 1:
+Entrada	    Saída
+2	        false
+Explicação: 2 possui apenas dois divisores: 1 e 2.
+
+*/
+
